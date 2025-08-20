45c2d76f559e7d93458dc7996d13ced3d69727c6
Kyle Ancheta <kyleancheta@live.com>
2025-08-19 22:50:25 -0700

    fixed some css shit

--- src/app/components-playground/page.jsx
+++ src/app/components-playground/page.jsx
@@ -8,27 +8,19 @@ import styles from './page.module.css'
 export default function ComponentsPlayground() {
   return (
     <main className={styles.main}>
-        <section className={`${styles.sectionContainer}`}>
-          <div className={`${styles.gridItem}`}></div>
-          <div className={`${styles.gridItem}`}>
+        <section className={`${styles.container}`}>
+          <div className={`${styles.gridItem} ${styles.gridHeader}`}>
             <h1 className={styles.heading}>Components Playground</h1>
             <p className={styles.description}>Recreating some components I see around the web.</p>
             </div>
-          <div className={`${styles.gridItem}`}></div>
           <div className={`${styles.gridItem}`}>
             <CleanButton>Blast off</CleanButton>
             <p className={styles.body}>Just a simple button with its states styled appropriately and a l'il active animation.</p>
-            {/* <TriSwitch/>
-            <p className={styles.body}>My first crack at the Figma toolbar state switcher.</p> */}
           </div>
           <div className={`${styles.gridItem}`}>
             <TriSwitch2/>
             <p className={styles.body}>My second crack at the Figma toolbar state switcher—motion's hard man, or maybe I'm just dumb.</p>
           </div>
-          <div className={`${styles.gridItem}`}></div>
-          <div className={`${styles.gridItem}`}></div>
-          <div className={`${styles.gridItem}`}></div>
-          <div className={`${styles.gridItem}`}></div>
         </section>
         <HomeButton/>
     </main>
